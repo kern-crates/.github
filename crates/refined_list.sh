88edf9434ffcafff433303b670e9664a08cae112
zjp <jiping_zhou@foxmail.com>
2025-07-09 17:42:49 +0800

    refined_list.sh: add drivercraft/driver-crates and fix grep regex

--- crates/refined_list.sh
+++ crates/refined_list.sh
@@ -2,6 +2,6 @@
 export LC_ALL=C
 
 # Extract user/repo from .gitmodules
-cat arceos-crates/.gitmodules starry-crates/.gitmodules axvisor-crates/.gitmodules |
-  grep -oP 'url = https:\/\/github\.com\/\K([^\/]+\/[^\/]+)(?=\.git)' |
+cat arceos-crates/.gitmodules starry-crates/.gitmodules axvisor-crates/.gitmodules driver-crates/.gitmodules |
+  grep -oP 'url = https:\/\/github\.com\/\K([^\/]+\/[^\/]+?)(?=\.git|\/|$)' |
   sort | uniq >refined_list.txt
