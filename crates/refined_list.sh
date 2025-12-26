e2909edf24beb87dc035cf71d49da75b83adbbb3
zjp <jiping_zhou@foxmail.com>
2025-12-26 09:34:14 +0000

    refined_list: refactor with awk

--- crates/refined_list.sh
+++ crates/refined_list.sh
@@ -7,9 +7,12 @@ export LC_ALL=C
 
 # Extract user/repo from .gitmodules
 cat arceos-crates/.gitmodules starry-crates/.gitmodules axvisor-crates/.gitmodules driver-crates/.gitmodules |
-  grep -oP 'url = https:\/\/github\.com\/\K([^\/]+\/[^\/]+?)(?=\.git|\/|$)' \
-    -v arceos-hypervisor/axcpu |
-  sort | uniq >refined_list.txt
+  awk -F'=' '/url = https:\/\/github\.com\// {
+    match($2, /([^\/]+\/[^\/]+?)(?=\.git|\/|$)/, arr);
+    if (arr[1] != "arceos-hypervisor/axcpu") {
+        print arr[1]
+    }
+  }' | sort | uniq >refined_list.txt
 
 # Add extra repos.
 echo "arceos-org/arceos" >>refined_list.txt
